3b75edd77ceb8bc5239c4d08f4ebb27fc1536918
Gabriel Monteiro Magan <111460258+GabrielMagan@users.noreply.github.com>
2022-12-19 22:25:29 -0300

    Add files via upload

---
+++ maquete/Fita_WRGB/Fita_WRGB.ino
@@ -0,0 +1,17 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN 2 
+#define NUMPIXELS 20
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup(){
+  pixels.begin();
+   for(int i = 0; i <= NUMPIXELS; i++){
+    pixels.setPixelColor(i, pixels.Color(255, 0, 35));
+    pixels.show();
+    delay(10);
+ }
+}
+void loop(){
+  delay(1000);
+}
